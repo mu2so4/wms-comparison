53886f5739d194aea6c248eb9041893800a1bd14
mu2so4 <b4.orangutang@gmail.com>
2025-06-01 15:57:18 +0700

    Remove time measuring in pure run

--- pure/pure_workflow.py
+++ pure/pure_workflow.py
@@ -1,5 +1,4 @@
 import sys
-import time
 
 parent_dir = ".."
 sys.path.append(parent_dir)
@@ -10,15 +9,6 @@ import task2
 if __name__ == '__main__':
     intermediate_path = 'filtered.sd'
     print("Running task 1")
-    start = time.process_time()
     task1.StageOne(outPath = intermediate_path)
-    task1_end = time.process_time()
     print("Running task 2")
     task2.StageTwo(intermediate_path)
-    task2_end = time.process_time()
-
-    print("Elapsed time:")
-    print(f"Task1: {(task1_end - start):.3f} s")
-    print(f"Task2: {(task2_end - task1_end):.3f} s")
-    print(f"Total: {(task2_end - start):.3f} s")
-    
