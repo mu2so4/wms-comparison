b342566d382739c9540e937e3b5991ce0be10fee
mu2so4 <b4.orangutang@gmail.com>
2025-06-03 21:07:43 +0700

    Fix Singularity script reading issue

--- pure/docker/run.sh
+++ pure/docker/run.sh
@@ -7,10 +7,10 @@ TASK2_OUT=$PWD/out-task2
 docker run \
     -v ${INPUT_PATH}:/app/data \
     -v ${TASK1_OUT}:/app/outputs \
-    mu2so4/seismic-filter-task:1.0.1 \
+    mu2so4/seismic-filter-task:1.0.2 \
     /app/data/input.sgy 15 30 20 /app/outputs/interm.sd && \
 docker run \
     -v ${TASK1_OUT}:/app/data \
     -v ${TASK2_OUT}:/app/outputs \
-    mu2so4/seismic-processing-task:1.0.1 \
+    mu2so4/seismic-processing-task:1.0.2 \
     /app/data/interm.sd 40 50 /app/outputs/pic.png /app/outputs/result.segy
--- pure/singularity/run.sh
+++ pure/singularity/run.sh
@@ -7,11 +7,11 @@ TASK2_OUT=$PWD/out-task2
 singularity run \
     --mount type=bind,src=${INPUT_PATH},dst=/app/data,ro \
     --mount type=bind,src=${TASK1_OUT},dst=/app/outputs \
-    ../../containers/docker-task1/task1-1.0.1.sif \
+    ../../containers/docker-task1/task1-1.0.2.sif \
     /app/data/input.sgy 15 30 20 /app/outputs/interm.sd && \
     
 singularity run \
     --mount type=bind,src=${TASK1_OUT},dst=/app/data,ro \
     --mount type=bind,src=${TASK2_OUT},dst=/app/outputs \
-    ../../containers/docker-task2/task2-1.0.1.sif \
+    ../../containers/docker-task2/task2-1.0.2.sif \
     /app/data/interm.sd 40 50 /app/outputs/pic.png /app/outputs/result.segy
