86bd264bca794e7dff639394505f1383afea2d55
mu2so4 <b4.orangutang@gmail.com>
2025-06-20 20:39:27 +0700

    Fix Action issues

--- pure/singularity/run.sh
+++ pure/singularity/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $(dirname "$0")
+
 INPUT_PATH=$PWD/../../inputs
 TASK1_OUT=$PWD/out-task1
 TASK2_OUT=$PWD/out-task2
@@ -7,11 +9,11 @@ TASK2_OUT=$PWD/out-task2
 singularity run \
     --mount type=bind,src=${INPUT_PATH},dst=/app/data,ro \
     --mount type=bind,src=${TASK1_OUT},dst=/app/outputs \
-    ../../containers/docker-task1/task1-1.0.2.sif \
+    docker://mu2so4/seismic-filter-task:1.0.2 \
     /app/data/input.sgy 15 30 20 /app/outputs/interm.sd && \
     
 singularity run \
     --mount type=bind,src=${TASK1_OUT},dst=/app/data,ro \
     --mount type=bind,src=${TASK2_OUT},dst=/app/outputs \
-    ../../containers/docker-task2/task2-1.0.2.sif \
+    docker://mu2so4/seismic-processing-task:1.0.2 \
     /app/data/interm.sd 40 50 /app/outputs/pic.png /app/outputs/result.segy
