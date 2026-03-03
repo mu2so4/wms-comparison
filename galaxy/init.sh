90479170b43ba55223235130d04b2ea73575395d
ilya <ibaranovscience@gmail.com>
2026-03-03 18:32:26 +0200

    Add galaxy

---
+++ galaxy/init.sh
@@ -0,0 +1,11 @@
+echo "Clonning galaxy git repository"
+git clone https://github.com/galaxyproject/galaxy.git
+
+echo "Copping tools conifguration"
+cp tool_conf.xml.sample ./galaxy/config/
+
+echo "Copping galaxy tools"
+cp -r seismic ./galaxy/tools
+
+echo "Run galaxy"
+sh ./galaxy/run.sh
